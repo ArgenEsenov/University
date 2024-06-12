1059a901bdaf40d720130f1914609c51a858f9f9
arlen <arlenismailov02@gmail.com>
2024-06-12 10:04:44 +0600

    swagger, UserPROFILE

--- mysite/university/models.py
+++ mysite/university/models.py
@@ -2,6 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
+    image = models.ImageField(upload_to='userprofile/img/', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.first_name} | {self.last_name}'
+
+
 class Faculty(models.Model):
     name = models.CharField(max_length=20)
     description = models.TextField()
